648d26c9ee6fc142aaf702bc55e0843152308ab8
Wittawas Nakkasem <vittee@hotmail.com>
2023-03-12 02:14:59 +0700

    Use separate Opus decoder for each receiver stream

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import { pipeline, Transform } from 'node:stream';
 import { Client, GatewayIntentBits } from 'discord.js';
 import { joinVoiceChannel, EndBehaviorType, AudioPlayerStatus, StreamType, createAudioPlayer, createAudioResource, getVoiceConnection, NoSubscriberBehavior, PlayerSubscription, AudioReceiveStream } from "@discordjs/voice";
 import pkg from '@discordjs/opus';
@@ -40,9 +41,16 @@ client.on("messageCreate", (message) => {
         }
         vcMembers.push(preparedMember);
     });
-    
-    const encoder = new OpusEncoder(48000, 2);
-    let mixer = new AudioMixer.Mixer({channels: 2, bitDepth: 16, sampleRate: 48000, clearInterval: 250});
+
+    // const encoder = new OpusEncoder(48000, 2);
+    let mixer = new AudioMixer.Mixer({ channels: 2, bitDepth: 16, sampleRate: 48000, clearInterval: 250 });
+
+    for (const user of receiver.speaking.users.keys()) {
+        if (!speakingUsers.includes(user)) {
+            playStream(playStream)
+        }
+        
+    }
 
     receiver.speaking.on("start", (user) => {
         if(speakingUsers.includes(user)) return;
@@ -66,22 +74,42 @@ client.on("messageCreate", (message) => {
 
     function playStream(userId) {
         if(client.users.cache.filter(x => x.bot && x.id == userId).first()) return;
-        const audioStream = receiver.subscribe(userId, { end: { behavior: EndBehaviorType.AfterSilence, duration: 100 } });
+        const audioStream = receiver.subscribe(userId, { end: { behavior: EndBehaviorType.AfterInactivity, duration: 100 } });
+
         const input = mixer.input({ volume: 75 });
-        speakingUsers.push(userId);
-    
+        speakingUsers.push(userId);    
+
+        const opus = new OpusEncoder(48000, 2);
+
+        // pipeline(audioStream, new OpusDecoderStream(new OpusEncoder(48000, 2)), new BufferedStream(4), input, (err) => {
+        //     if (err) {
+                
+        //     }
+        // });
+
+        const dataHandler = (chunk) => {
+            input.write(opus.decode(chunk));
+        }
+
+        const errorHandler = (err) => {
+            console.log(err)
+        }
+
+        const endHandler = () => {
+            console.log('userId', userId, 'end');
+
+            audioStream.off('data', dataHandler);
+            audioStream.off('error', errorHandler);
+
+            audioStream.destroy();
+            mixer.removeInput(input);
+            speakingUsers = speakingUsers.filter(x => x != userId);       
+        }
+
         audioStream
-            .on("data", (chunk) => {
-                input.write(encoder.decode(chunk));
-            })
-            .on("error", (err) => {
-                console.log(err)
-            })
-            .on("end", () => {
-                audioStream.destroy();
-                mixer.removeInput(input);
-                speakingUsers = speakingUsers.filter(x => x != userId);
-            });
+            .on("data", dataHandler)
+            .on("error", errorHandler)
+            .once("end", endHandler);
     }
 
     function initAudioPlayer() {
@@ -102,3 +130,39 @@ client.on("messageCreate", (message) => {
 process.removeAllListeners('warning'); // I'm sorry for adding this but I kept getting warnings from the part of code with the 'stateChange' event on line 52. I believe the way I used the mixer was wrong
 
 client.login(process.env.TOKEN);
+
+class OpusDecoderStream extends Transform {
+    encoder;
+
+    constructor(encoder) {
+        super({
+            writableObjectMode: true,
+            readableObjectMode: false
+        });
+        this.encoder = encoder;
+    }
+
+    _transform(opus, encoding, done) {
+        this.push(this.encoder.decode(opus));
+        done();
+    }
+}
+
+class BufferedStream extends Transform {
+    constructor(numBlocks) {
+        super();
+
+        this.numBlocks = numBlocks;
+        this.blocks = [];
+    }
+
+    _transform(pcm, encoding, done) {
+        this.blocks.push(pcm);
+
+        while (this.blocks.length >= this.numBlocks) {
+            this.push(this.blocks.shift());
+        }
+
+        done();
+    }
+}
